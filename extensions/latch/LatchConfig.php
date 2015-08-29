2181a31353a13dddb005d814613f48c507b87bc2
Paula <paula.rm@gmail.com>
2015-08-29 23:10:01 +0200

    Minor change - documentation file

--- extensions/latch/LatchConfig.php
+++
@@ -1,5 +0,0 @@
-<?php
-class LatchConfig {
-	const appId = "dc2szwUT3YGQLDHEEugA";  //unique application ID to Latch MediaWiki sotware
-	const secret = "eeuVQTRaV2DVdYL4vhGdF4QN98fPY6ED7FGzXdVr"; //unique secret key to Latch MediaWiki software
-}
